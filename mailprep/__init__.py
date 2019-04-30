69618e93d972bb2bff976c4bde27bb063258bfe0
James Payne <github@jepaynedev.com>
2019-04-30 07:16:41 -0500

    Add logging configuration and setup

--- mailprep/__init__.py
+++ mailprep/__init__.py
@@ -1,10 +1,27 @@
 import sys
+import logging
 from PySide2.QtWidgets import QApplication
 from mailprep.view.mainwindow import MainWindow
 
-def main():
-    app = QApplication(sys.argv)
+def setup_logger(level):
+    """Define logger at root of main module so all submodules inherit"""
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)  # Define lowest handled (not output)
 
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(level)
+
+    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+
+
+def main():
+    setup_logger(logging.DEBUG)
+
+    # Init and show interface
+    app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
 
--- mailprep/view/mainwindow.py
+++ mailprep/view/mainwindow.py
@@ -1,9 +1,14 @@
+import logging
 from PySide2.QtWidgets import QMainWindow
 from PySide2.QtCore import QFile
 from mailprep.ui.mainwindow_ui import Ui_MainWindow
 
+log = logging.getLogger(__name__)
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        log.debug('Loading MainWindow')
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
