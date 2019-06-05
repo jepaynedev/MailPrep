0646f057fa74d5e6ebcec5231607f37acd8709c2
James Payne <github@jepaynedev.com>
2019-06-05 00:35:18 -0500

    Add dev configuration with testing support
    * Add dev installation configuration installing pytest and tox
    * Add general setup.py updated information

--- setup.py
+++ setup.py
@@ -1,14 +1,51 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import find_packages
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 requires = [
     'PySide2',
 ]
 
+dev_extras = [
+    'pytest',
+    'tox',
+    'tox-venv',
+]
+
 setup(
     name='mailprep',
+    version='0.0',
+    description='List preparation for UW Extension Bulk Mail Center',
     author='James Payne',
     author_email='contact@jepaynedev.com',
+    license='MIT',
+    python_requires='>=3.4',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Win32',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
     install_requires=requires,
+    extras_require={
+        'dev': dev_extras,
+    },
     entry_points={
         'console_scripts': [
             'mailprep = mailprep:main',
