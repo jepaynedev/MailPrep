830d52c27e75a2ab449251e5dc05f850dac6e6da
James Payne <github@jepaynedev.com>
2019-10-13 13:49:43 -0500

    Add docstring to test

--- tests/test_module_import.py
+++ tests/test_module_import.py
@@ -4,5 +4,6 @@ import unittest
 class TestModuleImport(unittest.TestCase):
 
     def test_import_mailprep(self):
+        """Simply tries to import mailprep module then assert True. If import fails then exception will be thrown and test will fail."""
         import mailprep
         self.assertTrue(True)
